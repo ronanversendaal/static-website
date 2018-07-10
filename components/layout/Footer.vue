3fa59cc33397e9dfb36cb23e43f3e61f8b0047d1
Ronan Versendaal <2793483+ronanversendaal@users.noreply.github.com>
2018-07-10 11:35:47 +0200

    Inverse colors of footer, adds titles to anchors

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,14 +1,14 @@
 <template>
     <footer>
-        <div class="hero is-primary">
+        <div class="hero">
             <div class="hero-body">
                 <div class="container">
                     <div class="content social-links has-text-centered">
-                        <a target="_blank" href="https://www.linkedin.com/in/ronan-versendaal-23925380/" rel="noopener">
+                        <a target="_blank" title="My Linkedin page" href="https://www.linkedin.com/in/ronan-versendaal-23925380/" rel="noopener">
                             <i class="fa rounded fa-linkedin"></i>
                         </a>
 
-                        <a target="_blank" href="https://github.com/ronanversendaal" rel="noopener">
+                        <a target="_blank" title="My Github page" href="https://github.com/ronanversendaal" rel="noopener">
                             <i class="fa rounded fa-github"></i>
                         </a>
                     </div>
@@ -17,8 +17,3 @@
         </div>
     </footer>
 </template>
-<style>
-footer .social-links > a{
-    margin: 0 7px
-}
-</style>
